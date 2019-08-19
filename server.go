20190ceb1788e0c2a63b81cbce04dcbfe45ec9cb
Gopi <gopi.nanthakumaar@turner.com>
2019-08-19 09:40:02 +0100

    add aws secret

--- server.go
+++ server.go
@@ -10,32 +10,34 @@ import (
 	"strings"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var requests []string
-		log.Println("Processing request from ", r.Host)
-		hostName, _ := os.Hostname()
-		address, _ := net.InterfaceAddrs()
+func echoHeader(w http.ResponseWriter, r *http.Request) {
+	var requests []string
+	log.Println("HostName: ", r.Host)
+	hostName, _ := os.Hostname()
+	address, _ := net.InterfaceAddrs()
 
-		for _, address := range address {
-			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-				if ipnet.IP.To4() != nil {
-					requests = append(requests, fmt.Sprintf("PodIP:%v", ipnet.IP.String()))
-					break
-				}
+	for _, address := range address {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				requests = append(requests, fmt.Sprintf("PodIP:%v", ipnet.IP.String()))
+				break
 			}
 		}
-		requests = append(requests, fmt.Sprintf("HostName:%v", hostName))
-		for name, headers := range r.Header {
-			for _, val := range headers {
-				requests = append(requests, fmt.Sprintf("%v:%v", name, val))
-			}
+	}
+	requests = append(requests, fmt.Sprintf("HostName:%v", hostName))
+	for name, headers := range r.Header {
+		for _, val := range headers {
+			requests = append(requests, fmt.Sprintf("%v:%v", name, val))
 		}
-		requests = append(requests, fmt.Sprintf("Tag:%v", "test"))
-		requests = append(requests, "Whitelist:True")
-		sort.Strings(requests)
-		fmt.Fprintf(w, strings.Join(requests, "\n"))
-	})
+	}
+	requests = append(requests, fmt.Sprintf("Tag:%v", "test"))
+	requests = append(requests, "Whitelist:True")
+	sort.Strings(requests)
+	fmt.Fprintf(w, strings.Join(requests, "\n"))
+}
+
+func main() {
+	http.HandleFunc("/", echoHeader)
 	log.Println("Starting server and listening on 8080")
 	http.ListenAndServe(":8080", nil)
 }
